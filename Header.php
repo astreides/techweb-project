944b6478b217e9ba04b5d854aef869fc0a6960f7
JustinDuban <justin.duban@free.fr>
2018-10-05 13:23:33 +0200

    header tjrs plus

--- Header.php
+++ Header.php
@@ -1,8 +1,11 @@
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+function techno(){
+	<a href="page.html"/a>
+}
 <header>
 	<center>Site de vente.com</center>
 	<log>
-    <form name="affiche" method="post" action="www.techno-web.fr">
+    <form name="affiche" method="post" action="techno()">
         login : <input type="text" name="login" />
         password : <input type="text" name="password" />
         <input type="submit" value="OK">

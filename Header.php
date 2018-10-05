9c44bc7405686aac422680483588123672d95d21
JustinDuban <justin.duban@free.fr>
2018-10-05 12:59:39 +0200

    ko

--- Header.php
+++ Header.php
@@ -1,4 +1,5 @@
-<heaser>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+<header>
     <form name="affiche" method="post" action="table/table.html">
         login : <input type="text" name="login" />
         password : <input type="text" name="password" />

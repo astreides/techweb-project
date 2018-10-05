bde9ffe946ed3d3cbcecd01caa9935e8a71363e2
Louis THIÉRY <louis.thiery@isen.yncrea.fr>
2018-10-05 15:56:59 +0200

    retrait du tableau

--- Accueil.php
+++ Accueil.php
@@ -8,22 +8,7 @@
         
         </style>
     </head>
-    <nav><style>
-        table{border-collapse: collapse}
-        td,th{border: 1px solid black;}
-    </style>
-            <table>
-                <th><titre>T-shirts</titre><br/></th></tr>
-                <th><a href='T-shirt vert.php'> Green T-shirt</a><br/>
-                    <a href='T-shirt jaune.php'> Yellow T-shirt</a><br/></th></tr>
-                    <th><titre> pantalon</titre><br/></th></tr>
-                    <th> <a href='Pantalon vert.php'> Green pantalon</a><br/>
-                    <a href='Pantalon bleu.php'> Blue pantalon</a><br/></th></tr>
-                <th><titre> shoes</titre><br/></th></tr>
-                    <th><a href='Chaussures noires.php'> black shoes</a><br/>
-                    <a href='Chaussures bleues.php'> blue shoes</a><br/></tr>
-            </table>
-    </nav>
+
     <body>
 	<?php include("Header.php") ?>
 <section>

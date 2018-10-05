75faa6c5d190a2e641d368cdbf28b5bd49d0faae
THIERY Louis <35230032+noirock@users.noreply.github.com>
2018-10-05 13:50:34 +0200

    Update Header.php

--- Header.php
+++ Header.php
@@ -1,12 +1,12 @@
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 
-<header>
-	<center>Site de vente.com</center>
-	<log style="text-align:right">
+<log style="text-align:right">
     <form name="affiche" method="post" action="https://www.techno-web.fr/index.php">
         login : <input type="text" name="login" />
         password : <input type="text" name="password" />
         <input type="submit" value="OK">
     </form>
-	</log>
+</log>
+<header>
+	<center>Site de vente.com</center>
 </header>

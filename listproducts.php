19126e92a6bf1dfa8886c4c17b9e44556aad7238
JustinDuban <justin.duban@free.fr>
2018-10-05 16:24:37 +0200

    prodju

--- listproducts.php
+++ listproducts.php
@@ -6,5 +6,29 @@
 </head>
 <body>
     <?php include("Header.php") ?>
+	    <table>
+        <caption>Products</caption>
+        <thead><tr>
+            <th>Categorie</th>
+            <th>Picture</th>
+            <th>Cuteness</th>
+            <th>See more</th>
+            </tr></thead>
+        <tfoot><tr>
+            <td><br /></td>
+            <td colspan="3">Footer<br /></td>
+            </tr></tfoot>
+        <tbody><tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            </tr><tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                </tr></tbody>
+    </table>
 </body>
 </html>

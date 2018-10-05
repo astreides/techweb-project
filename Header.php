f5206e46852a5f7c3aa6abb6b34bccbd6dc55204
JustinDuban <justin.duban@free.fr>
2018-10-05 13:06:26 +0200

    header encore

--- Header.php
+++ Header.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 <header>
-	<title>Hello, <?php echo $inputName?></title>
+	<center>Site de vente.com</center>
     <form name="affiche" method="post" action="table/table.html">
         login : <input type="text" name="login" />
         password : <input type="text" name="password" />

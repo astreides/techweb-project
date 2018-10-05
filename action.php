a70f53ae2ad60957d051f3b120c7e9c6832e2d54
astreides <35225989+astreides@users.noreply.github.com>
2018-10-05 16:18:28 +0200

    action.php

--- action.php
+++ action.php
@@ -10,5 +10,8 @@
 	login: <?php echo htmlspecialchars($_POST["nlogin"]); ?></br>
 	password: <?php echo htmlspecialchars($_POST["npassword"]); ?></br>
 	<a href='Accueil'> Retour Menu</a>
+<center><img src='images/bb ours.jpg'alt='T-shirts' width"500px"/></center>
+<center><img src='images/bb tigre.jpg'alt='T-shirts' width"500px"/></center>
+<center><img src='images/loutre.jpg'alt='T-shirts' width"500px"/></center>
 </body>
 </html>

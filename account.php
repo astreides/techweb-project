80d6c3d6bc55621509b55c3e288636b8fc4ab073
astreides <35225989+astreides@users.noreply.github.com>
2018-10-05 14:33:32 +0200

    account

--- account.php
+++ account.php
@@ -1,18 +1,16 @@
 <!DOCTYPE>
-<form name="affiche" method="post" action="acceuil.php">
-	login : <input type="text" name=" new  login" />
-        password : <input type="text" name=" new password" />
-     	<input type="submit" value="OK">
-</form>
+
 <html>
 <head>
     <meta charset="iso-8859-1" />
     <title>Cart</title>
 </head>
-<header>
-	<center>Site de vente.com</center>
-</header>
 <body>
     <?php include("Header.php") ?>
+<form name="affiche" method="post" action="acceuil.php">
+	login : <input type="text" name=" new  login" />
+        password : <input type="text" name=" new password" />
+     	<input type="submit" value="OK">
+</form>
 </body>
 </html>

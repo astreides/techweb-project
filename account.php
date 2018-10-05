c25e08afc080301945ebc01631e9b76620cc2eaf
astreides <35225989+astreides@users.noreply.github.com>
2018-10-05 16:41:14 +0200

    change pour sql

--- account.php
+++ account.php
@@ -6,10 +6,16 @@
     <title>create account page</title>
 </head>
 <body>
-<form method="post" action="action.php">
-	new login : <input type="text" name="nlogin" />
-        new password : <input type="password" name="npassword" />
-     	<input type="submit" value="OK">
+<form method="post" action="action.php"></br>
+	new login : <input type="text" name="nlogin" /></br>
+        new password : <input type="password" name="npassword" /></br>
+	nom de ta maman : <input type="text" name="mama" /></br>
+        nom de ton papa : <input type="text" name="papa" /></br>
+	ton petit nom : <input type="text" name="name" /></br>
+        ta pitite adress : <input type="text" name="adress" /></br>
+	ton pitit 06 : <input type="text" name="tel" /></br>
+	ton age : <input type="text" name="age" /></br>
+     	<input type="submit" value="OK"></br>
 </form>
 </body>
 </html>
--- action.php
+++ action.php
@@ -9,6 +9,12 @@
 <body>
 	login: <?php echo htmlspecialchars($_POST["nlogin"]); ?></br>
 	password: <?php echo htmlspecialchars($_POST["npassword"]); ?></br>
+	nom de ta maman: <?php echo htmlspecialchars($_POST["mama"]); ?></br>
+	nom de ton papa: <?php echo htmlspecialchars($_POST["papa"]); ?></br>
+	ton petit nom: <?php echo htmlspecialchars($_POST["name"]); ?></br>
+	ta pitite adress: <?php echo htmlspecialchars($_POST["adress"]); ?></br>
+	ton pitit 06: <?php echo (int)$_POST["tel"]; ?></br>
+	ton age: <?php echo (int)$_POST["age"]; ?></br>
 	<a href='Accueil'> Retour Menu</a>
 <center><img src='images/bb ours.jpg'alt='T-shirts' width"500px"/></center>
 <center><img src='images/bb tigre.jpg'alt='T-shirts' width"500px"/></center>

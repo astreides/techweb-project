ced50c03c5e34da08494cbdd1a37c089fef4183b
JustinDuban <justin.duban@free.fr>
2018-10-05 13:11:00 +0200

    header encore2

--- Header.php
+++ Header.php
@@ -1,9 +1,11 @@
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 <header>
 	<center>Site de vente.com</center>
+	<log>
     <form name="affiche" method="post" action="table/table.html">
         login : <input type="text" name="login" />
         password : <input type="text" name="password" />
         <input type="submit" value="OK">
     </form>
+	</log>
 </header>

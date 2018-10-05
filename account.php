2f7e43641c1d361c31243979d949d2e9f38cca4f
astreides <35225989+astreides@users.noreply.github.com>
2018-10-05 14:36:04 +0200

    account

--- account.php
+++ account.php
@@ -3,10 +3,9 @@
 <html>
 <head>
     <meta charset="iso-8859-1" />
-    <title>Cart</title>
+    <title>create account page</title>
 </head>
 <body>
-    <?php include("Header.php") ?>
 <form name="affiche" method="post" action="acceuil.php">
 	login : <input type="text" name=" new  login" />
         password : <input type="text" name=" new password" />

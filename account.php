efce9700e2c2cabea886b6238c7f38e8adf2cdd8
astreides <35225989+astreides@users.noreply.github.com>
2018-10-05 17:05:56 +0200

    ^voila tm

--- account.php
+++ account.php
@@ -1,4 +1,4 @@
-<!DOCTYPE>
+﻿<!DOCTYPE>
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 <html>
 <head>
@@ -8,14 +8,14 @@
 <body>
 <form method="post" action="action.php"></br>
 <table>
-    <th>Pseudo : <input type="text" name="nlogin" /></br></th><tr>
+    <th>Pseudo^^ : <input type="text" name="nlogin" /></br></th><tr>
     <th>mot de passe : <input type="password" name="npassword" /></br></th><tr>
     <th>confirmation : <input type="password" name="validation" /></br></th><tr>
     <th>nom : <input type="password" name="nom" /></br></th><tr>
     <th>prénom : <input type="password" name="prenom" /></br></th><tr>
     <th>adresse : <input type="text" name="adresse" /></br></th><tr>
-    <th>numéro de tépéphone : <input type="text" name="tel" /></br></th><tr>
-	<th>date de naissance : <input type="text" name="age" /></br></th><tr>
+    <th>numéro de pédéphone : <input type="text" name="tel" /></br></th><tr>
+	<th>date de naissance^^ : <input type="text" name="age" /></br></th><tr>
     <th><input type="submit" value="OK"></br></th><tr>
 </table>
 

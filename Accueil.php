30f4c35655e5474444768ccbe3767813c407597c
THIERY Louis <35230032+noirock@users.noreply.github.com>
2018-10-05 12:52:13 +0200

    Add files via upload

---
+++ Accueil.php
@@ -0,0 +1,40 @@
+<!DOCTYPE php>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+<html>
+    <head>
+        <title>Site de vente.com</title>
+        <header>
+            <center>Site de vente.com</center>
+            
+        </header>
+        <style>
+        
+        </style>
+    </head>
+    <nav><style>
+        table{border-collapse: collapse}
+        td,th{border: 1px solid black;}
+    </style>
+            <table>
+                <th><titre>T-shirts</titre><br/></th></tr>
+                <th><a href='T-shirt vert.php'> Green T-shirt</a><br/>
+                    <a href='T-shirt jaune.php'> Yellow T-shirt</a><br/></th></tr>
+                    <th><titre> pantalon</titre><br/></th></tr>
+                    <th> <a href='Pantalon vert.php'> Green pantalon</a><br/>
+                    <a href='Pantalon bleu.php'> Blue pantalon</a><br/></th></tr>
+                <th><titre> shoes</titre><br/></th></tr>
+                    <th><a href='Chaussures noires.php'> black shoes</a><br/>
+                    <a href='Chaussures bleues.php'> blue shoes</a><br/></tr>
+            </table>
+    </nav>
+    <body>
+<section>
+    <center><img src='images/pubTshirt.jpeg'alt='T-shirts' width"500px"/></center>
+</section>
+
+
+    </body>
+    <footer>
+        <center>vente.com</center>
+    </footer>
+</html>
---
+++ Chaussures bleues.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+<html>
+    <head>
+        <title>Site de vente.com</title>
+        <header>
+            <center>Site de vente.com</center>
+        </header>
+        <style>
+        
+        </style>
+    </head>
+    <nav><style>
+        table{border-collapse: collapse}
+        td,th{border: 1px solid black;}
+    </style>
+            <table>
+                <th><titre>T-shirts</titre><br/></th></tr>
+                <th><a href='T-shirt vert.php'> Green T-shirt</a><br/>
+                    <a href='T-shirt jaune.php'> Yellow T-shirt</a><br/></th></tr>
+                    <th><titre> pantalon</titre><br/></th></tr>
+                    <th> <a href='Pantalon vert.php'> Green pantalon</a><br/>
+                    <a href='Pantalon bleu.php'> Blue pantalon</a><br/></th></tr>
+                <th><titre> shoes</titre><br/></th></tr>
+                    <th><a href='Chaussures noires.php'> black shoes</a><br/>
+                    <b href='Chaussures bleues.php'> blue shoes</b><br/></tr>
+                    <th><a href='Accueil.php'>Accueil</a></th>
+            </table>
+    </nav>
+    <body>
+<section>
+    <center><img src='images/blueShoes.jpeg'alt='chaussures' width"500spx"/></center>
+</section>
+<c>
+    <t>Description</t><br/><br/>
+    blablablablabla blablablablabla blablablablabla blablablablabla <br/>
+    blablablablabla <br/>
+    blablablablabla blablablablabla blablablablabla <br/><br/>
+    <t> prix unitaire TTC: </t>
+    <prix> 7€99</prix>
+
+<php>
+<form name="achat" method="post" action="Accueil.php">
+    Entrez la quantité: <input type="text" name="quantité"/> 
+    <input type="submit" name="valider" value="OK"/>
+</form>
+</php>
+</c>
+
+    </body>
+    <footer>
+        <center>vente.com</center>
+    </footer>
+</html>

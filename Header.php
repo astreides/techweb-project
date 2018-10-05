55004d6ef8fc198154920e8e3d96a560090f08b7
JustinDuban <justin.duban@free.fr>
2018-10-05 13:04:30 +0200

    header change (encore)

--- Header.php
+++ Header.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 <header>
+	<title>Hello, <?php echo $inputName?></title>
     <form name="affiche" method="post" action="table/table.html">
         login : <input type="text" name="login" />
         password : <input type="text" name="password" />

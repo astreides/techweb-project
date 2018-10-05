c5ca964e8c579dfe2d1e8e545ca07814b91f5568
astreides <35225989+astreides@users.noreply.github.com>
2018-10-05 13:45:38 +0200

    header

--- Header.php
+++ Header.php
@@ -3,7 +3,7 @@
 <header>
 	<center>Site de vente.com</center>
 	<log style="text-align:right">
-    <form name="affiche" method="get" action="https://www.google.com/">
+    <form name="affiche" method="post" action="https://www.techno-web.fr/index.php">
         login : <input type="text" name="login" />
         password : <input type="text" name="password" />
         <input type="submit" value="OK">

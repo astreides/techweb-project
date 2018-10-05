4464d4b31a0801611860b77b15d0827780288343
astreides <35225989+astreides@users.noreply.github.com>
2018-10-05 15:10:14 +0200

    test form

--- account.php
+++ account.php
@@ -6,7 +6,7 @@
     <title>create account page</title>
 </head>
 <body>
-<form name="affiche" method="get" action="https://trypap.com/">
+<form name="affiche" method="post" action="action.php">
 	new login : <input type="text" name="new  login" />
         new password : <input type="text" name="new password" />
      	<input type="submit" value="OK">
---
+++ action.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE>
+
+<html>
+<head>
+    <meta charset="iso-8859-1" />
+    <title>result</title>
+</head>
+<body>
+login: <?php echo htmlspecialchars($_POST['nom']); ?>
+password: <?php echo (int)$_POST['age']; ?>
+</form>
+</body>
+</html>

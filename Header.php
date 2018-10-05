bea23c0ffaf97deb5c2fc9e09996ef9e6c0c436f
astreides <35225989+astreides@users.noreply.github.com>
2018-10-05 13:41:10 +0200

    header get

--- Header.php
+++ Header.php
@@ -5,7 +5,7 @@ function techno(){
 <header>
 	<center>Site de vente.com</center>
 	<log style="text-align:right">
-    <form name="affiche" method="post" action="https://www.google.com/">
+    <form name="affiche" method="get" action="https://www.google.com/">
         login : <input type="text" name="login" />
         password : <input type="text" name="password" />
         <input type="submit" value="OK">

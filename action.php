afa1fb8653e284c27736807c48af8548b7c200d6
JustinDuban <justin.duban@free.fr>
2018-10-11 14:58:41 +0200

    ju

--- action.php
+++ action.php
@@ -14,7 +14,7 @@
 	ta pitite adresse: <?php echo htmlspecialchars($_POST["adresse"]); ?></br>
 	ton pitit 06: <?php echo (int)$_POST["tel"]; ?></br>
 	date de naissance: <?php echo (int)$_POST["age"]; ?></br>
-	<a href='Accueil'> Retour Menu</a>
+	<a href='Accueil.php'> Retour Menu</a>
 <center><img src='images/bb ours.jpg'alt='T-shirts' width"500px"/></center>
 <center><img src='images/bb tigre.jpg'alt='T-shirts' width"500px"/></center>
 <center><img src='images/loutre.jpg'alt='T-shirts' width"500px"/></center>

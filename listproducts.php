630dc8ac577486bac03042b0834280c75d16ad2d
ClementNouge <clement.nouge@isen.yncrea.fr>
2018-10-12 14:43:33 +0200

    Update listproducts.php

--- listproducts.php
+++ listproducts.php
@@ -5,7 +5,11 @@
     <title>Produits</title>
 </head>
 <body>
-    <?php include("Header.php") ?>
+    <?php
+    include("Header.php");
+    $reponse=$bdd->query('SELECT * FROM products');
+    
+    ?>
 <form method="post" action="product.php">
 	    <table border=6 cellspacing=12 cellpadding=20>
         <thead><tr>
@@ -14,26 +18,21 @@
             <th>Cuteness</th>
             <th>See more</th>
             </tr></thead>
-        <tfoot><tr>
-            <td><br /></td>
-            <td colspan="3">Footer<br /></td>
-            </tr></tfoot>
-        <tbody><tr>
-            <td>Tigre</td>
-            <td><image src='images/bb tigre.jpg' width="50px"></td>
-            <td>300</td>
-            <td><input type="submit" name="produits" value="tigre"></td>
-            </tr><tr>
-                <td>Ours</td>
-                <td><image src='images/bb ours.jpg' width="50px"></td>
-                <td>279</td>
-                <td><input type="submit" name="produits" value="ours"></td>
-                </tr><tr>
-					<td>Loutre</td>
-					<td><image src='images/loutre.jpg' width="50px"></td>
-					<td>279</td>
-					<td><input type="submit" name="produits" value="loutre"></td>
-					</tr></tbody>
+        
+        <tbody>
+        	<?php while($donnees=$reponse->fetch())
+        	{
+        		?>
+        		<tr>
+            	<td> <?php echo $donnees['name'] ?></td>
+            	<td><image src='images/bb tigre.jpg' width="50px"></td>
+            	<td><?php echo $donnees['cutiness'] ?></td>
+            	<td><input type="submit" name="produits" value=<?php echo $donnees['name'] ?>></td>
+            	</tr>
+            	<?php
+        	}
+        	?>
+        </tbody>
     </table>
 </form>
 </body>

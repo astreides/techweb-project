adbfe1679c55966c9cfb8707831140e6734b4f46
astreides <35225989+astreides@users.noreply.github.com>
2018-10-12 14:29:50 +0200

    test

--- account.php
+++ account.php
@@ -6,19 +6,42 @@
     <title>create account page</title>
 </head>
 <body>
-<form method="post" action="action.php"></br>
+<form method="post" ></br>
+
+	mot de passe : <input type="password" name="nepassword" /></br>
+	confirmation : <input type="password" name="validation" /></br>
+ <input type="submit" value="OK" ></br>
+
+</form>
+<?php
+function maFonction(){
+    if( !isset($_POST["nepassword"]) ){
+        throw new Exception("param est vide");
+    }
+   if(strcmp($_POST["nepassword"],$_POST["validation"]) == 0){
+	?><html>
+	<form method="post" action="action.php"></br>
+	<input name="npassword" type="hidden" value="test">
 <table border=6 cellspacing=12 cellpadding=20>
     <th>Pseudo : <input type="text" name="nlogin" /></br></th><tr>
-    <th>mot de passe : <input type="password" name="npassword" /></br></th><tr>
-    <th>confirmation : <input type="password" name="validation" /></br></th><tr>
-    <th>nom : <input type="password" name="nom" /></br></th><tr>
-    <th>prénom : <input type="password" name="prenom" /></br></th><tr>
+    <th>nom : <input type="text" name="nom" /></br></th><tr>
+    <th>prénom : <input type="text" name="prenom" /></br></th><tr>
     <th>adresse : <input type="text" name="adresse" /></br></th><tr>
     <th>numéro de péléphone : <input type="text" name="tel" /></br></th><tr>
 	<th>date de naissance : <input type="text" name="age" /></br></th><tr>
     <th><input type="submit" value="OK"></br></th><tr>
 </table>
+	</form>
+	</html>
+	<?php	
+		}
+	}
+ 
+try{
+    maFonction();
+} catch(Exception $e){
 
-</form>
+}	
+?>
 </body>
 </html>

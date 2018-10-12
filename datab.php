1fd7dded8bd1669bb157082199e68f1d2bd13525
JustinDuban <justin.duban@free.fr>
2018-10-12 11:33:13 +0200

    data

--- datab.php
+++ datab.php
@@ -1,1 +1,10 @@
-<?php $bdd = new PDO('mysql:host=localhost;dbname=dump;charset=utf8','root','');?>
+<?php 
+try{
+$bdd = new PDO('mysql:host=localhost;dbname=dump;charset=utf8','root','');
+}
+catch(Exception $e)
+{
+		die('Erreur: ' . $e-getMessage());
+}
+$reponse=$bdd->query('SELECT * FROM orders');
+?>

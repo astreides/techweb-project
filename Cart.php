56e9f77791c3ac518094d03e356716dbbb38fb5f
JustinDuban <justin.duban@free.fr>
2018-10-12 18:21:24 +0200

    panier

--- Cart.php
+++ Cart.php
@@ -6,6 +6,35 @@
 </head>
 <body>
     <?php include("Header.php") ?></br>
-	Merci de votre commande vous avez commander <?php echo (int)$_POST["quantity"]; ?> petit animaux</br>
+	<?php
+	/*$reponse=$bdd->query('SELECT p.* FROM order_products p 
+	INNER JOIN order o  ON p.id=o.id
+	WHERE p.id=4') or die(print_r($bdd->errorInfo()));*/
+
+	$reponse=$bdd->query('select p.* from products p
+INNER JOIN order_products op ON p.id = op.product_id
+WHERE op.order_id = 4');
+
+	?>
+	<table>
+	<thead><tr>
+		<th>Categorie</th>
+        <th>Picture</th>
+        <th>Cuteness</th>
+        <th>Remove from Cart</th>
+        </tr></thead>
+	<tbody>
+		<?php while($donnees=$reponse->fetch()){
+			?>
+			<tr>
+            <td> <?php echo $donnees['name'] ?></td>
+            <td><image src=<?php echo $donnees['lien_image'] ?> width="200px"></td>
+            <td><?php echo $donnees['cutiness'] ?></td>
+            <td><input type="submit" name="produits" value="delete"></td>
+            </tr>
+			<?php
+		}?>
+	</tbody>
+	</table>
 </body>
 </html>

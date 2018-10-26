e6b78642dc8550e1b20283c4a1aaac5c1f2db4c4
ClementNouge <clement.nouge@isen.yncrea.fr>
2018-10-26 15:25:13 +0200

    Update view/product.php

--- view/product.php
+++ view/product.php
@@ -5,23 +5,7 @@
     <title>Cart</title>
 </head>
 
-	<?php
-	function add_to_cart($id_cart, $id_item, $quantity, $price){
-		echo $id_cart;
-		echo $id_item;
-		echo $quantity;
-		echo $price;
-		
-		
-		$req = $bdd->prepare('INSERT INTO `order_products` (order_id, product_id, quantity, unit_price) VALUES (:id_cart,:item,:quantity,:price)');
-		$req->execute(array(
-			'id_cart' => $id_cart,
-			'item' => $id_item,
-			'quantity' => $quantity,
-			'price' => $price,
-			));
-
-	} ?>
+	
 
 <body>
 
@@ -30,15 +14,16 @@
 	
 	$reponse=$bdd->query('SELECT * FROM products WHERE name=\''.$_POST["product"].'\'');
 
-	while ($donnees=$reponse->fetch()){
-		?>
-		<image src=<?php echo $donnees['lien_image'] ?> width="600px"></br>
-		<?php
-		echo $donnees['description']." ".$donnees['unit_price']." euros";
+	$donnees=$reponse->fetch()
+	?>
+	<image src=<?php echo $donnees['lien_image'] ?> width="600px"></br>
+	<?php
+	echo $donnees['description']." ".$donnees['unit_price']." euros";
 		
-	}
-		if( isset($_POST["quantity"]) ){
-        add_to_cart($_POST["num_cart"],  $donnees['id'], $_POST["quantity"],  $donnees['unit_price']);
+	
+	if( isset($_POST["quantity"]) ){
+        $req = $bdd->query('INSERT INTO `order_products` (order_id, product_id, quantity, unit_price) VALUES (\''.$_POST["num_cart"].'\',\''.$donnees['id'].'\',\''.$_POST["quantity"].'\',\''.$donnees['unit_price'].'\')');
+		
     }
 	?>
 

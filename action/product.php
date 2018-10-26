4a0ebf72c4b8680cc1a8acf843f46c0cc14a6b42
ClementNouge <clement.nouge@isen.yncrea.fr>
2018-10-26 16:54:16 +0200

    Update action/product.php

--- action/product.php
+++ action/product.php
@@ -3,8 +3,14 @@
 
 	$donnees=$reponse->fetch();
 	if( isset($_POST["quantity"]) ){
-        $req = $bdd->query('INSERT INTO `order_products` (order_id, product_id, quantity, unit_price) VALUES (\''.$_POST["num_cart"].'\',\''.$donnees['id'].'\',\''.$_POST["quantity"].'\',\''.$donnees['unit_price'].'\')');
-
+		
+        $req = $bdd->prepare('INSERT INTO `order_products` (order_id, product_id, quantity, unit_price) VALUES (:id_cart,:item,:quantity,:price)');
+		$req->execute(array(
+			'id_cart' => $_POST["num_cart"],
+			'item' => $donnees['id'],
+			'quantity' => $_POST["quantity"],
+			'price' => $donnees['unit_price'],
+			));
 		
     }
 	?>

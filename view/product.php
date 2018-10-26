b5c9253619be5ba50e7b95aef8fcc81169f51b3f
ClementNouge <clement.nouge@isen.yncrea.fr>
2018-10-26 15:29:13 +0200

    a

--- view/product.php
+++ view/product.php
@@ -24,7 +24,7 @@
 	if( isset($_POST["quantity"]) ){
         $req = $bdd->query('INSERT INTO `order_products` (order_id, product_id, quantity, unit_price) VALUES (\''.$_POST["num_cart"].'\',\''.$donnees['id'].'\',\''.$_POST["quantity"].'\',\''.$donnees['unit_price'].'\')');
 
-		azrzz
+		
     }
 	?>
 

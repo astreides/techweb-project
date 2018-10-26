4936c1cb2200b283854bf167402c8f5be2dcb67c
astreides <35225989+astreides@users.noreply.github.com>
2018-10-26 15:11:21 +0200

    product

--- view/product.php
+++ view/product.php
@@ -7,9 +7,11 @@
 
 	<?php
 	function add_to_cart($id_cart, $id_item, $quantity, $price){
-	if( !isset($_POST["quantity"]) ){
-        throw new Exception("param est vide");
-    }
+		echo $id_cart;
+		echo $id_item;
+		echo $quantity;
+		echo $price;
+		
 		
 		$req = $bdd->prepare('INSERT INTO `order_products` (order_id, product_id, quantity, unit_price) VALUES (:id_cart,:item,:quantity,:price)');
 		$req->execute(array(
@@ -22,6 +24,7 @@
 	} ?>
 
 <body>
+
     </br>
 	<?php
 	
@@ -34,16 +37,15 @@
 		echo $donnees['description']." ".$donnees['unit_price']." euros";
 		
 	}
-
+		if( isset($_POST["quantity"]) ){
+        add_to_cart($_POST["num_cart"],  $donnees['id'], $_POST["quantity"],  $donnees['unit_price']);
+    }
 	?>
 
 <form method="post">
 quantity voulue <input type="text" name="quantity"  /></br>
 <input type="hidden" name="product" value=<?php echo $_POST["product"] ?> >
-<input type="hidden" name="num_cart" value="4" ?> >
-<input type="hidden" name="ids" value="$donnees['id']" ?> >
-<input type="hidden" name="quantiter" value="quantity" ?> >
-<input type="hidden" name="prix" value="$donnees['unit_price']" >
+<input type="hidden" name="num_cart" value="4"  >
 <input type="submit" name="commande" >
 </form>
 </body>

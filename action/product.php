2a9704134c9903c75d062d3d311e400e23c2cfe0
JustinDuban <justin.duban@free.fr>
2018-11-10 00:39:51 +0100

    solution
    J'ai résolu le bug!!!

--- action/product.php
+++ action/product.php
@@ -1,18 +1,20 @@
 	<?php 
 	//action
-	$reponse=$bdd->query('SELECT * FROM products WHERE name=\''.$_POST["product"].'\'');
-	$donnees=$reponse->fetch();
-	$requete='SELECT * FROM order_products WHERE product_id=\''.$donnees["id"].'\'';
-	$cart=$bdd->query($requete);
-	$i =0;
-	if( isset($_POST["quantity"]) ){
-		echo $_POST["num_cart"];
-		while($panier =$cart->fetch()){
-			$req = $bdd->query('UPDATE `order_products` set `quantity`= `quantity`+\''.$_POST["quantity"].'\' WHERE product_id=\''.$donnees["id"].'\'AND order_id=\''.$_POST["num_cart"].'\'');
-			$i=1;
+	if( isset($_POST["num_cart"]) ){
+		$reponse=$bdd->query('SELECT * FROM products WHERE name=\''.$_POST["product"].'\'');
+		$donnees=$reponse->fetch();
+		$requete='SELECT * FROM order_products WHERE product_id=\''.$donnees["id"].'\'AND order_id=\''.$_POST["num_cart"].'\'';
+		$cart=$bdd->query($requete);
+		$i =0;
+		if( isset($_POST["quantity"]) ){
+			echo $_POST["num_cart"];
+			while($panier =$cart->fetch()){
+				$req = $bdd->query('UPDATE `order_products` set `quantity`= `quantity`+\''.$_POST["quantity"].'\' WHERE product_id=\''.$donnees["id"].'\'AND order_id=\''.$_POST["num_cart"].'\'');
+				$i=1;
+			}
+			if($i==0){
+				if($req = $bdd->query('INSERT INTO `order_products` (order_id, product_id, quantity, unit_price) VALUES (\''.$_POST["num_cart"].'\',\''.$donnees['id'].'\',\''.$_POST["quantity"].'\',\''.$donnees['unit_price'].'\')')){print "HAAAAAPPPPPPPPPYYYYYYYYYYYY"; }
+			}   
 		}
-		if($i==0){
-			if($req = $bdd->query('INSERT INTO `order_products` (order_id, product_id, quantity, unit_price) VALUES (\''.$_POST["num_cart"].'\',\''.$donnees['id'].'\',\''.$_POST["quantity"].'\',\''.$donnees['unit_price'].'\')')){print "HAAAAAPPPPPPPPPYYYYYYYYYYYY"; }
-		}   
 	}
 	?> 

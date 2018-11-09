5b0e9fa6a5d82ad87d87b782f3b5fbbaf8092f48
ClementNouge <clement.nouge@isen.yncrea.fr>
2018-11-09 15:10:58 +0100

    cart

--- action/Cart.php
+++ action/Cart.php
@@ -1,1 +1,9 @@
-<?php //action ?>
+<?php 
+
+if( isset($_POST["delete"]) ){
+		echo ($_POST["delete"]);
+        $req = $bdd->query('DELETE FROM `order_products` WHERE order_id=4 AND product_id=\''.$_POST["delete"].'\' ');
+
+		
+    }
+//action ?>

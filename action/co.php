1674874c28695f1cb7a0acead350fee73791447a
JustinDuban <justin.duban@free.fr>
2018-11-09 21:39:07 +0100

    verif co

--- action/co.php
+++ action/co.php
@@ -1,6 +1,11 @@
 #ici verifier que le $_POST["login"] et que le $_POST["password"] soit correspondant avec la bdd et si oui faite rentrer dans le if
-<?php 
-if(($_POST["login"] != NULL) && ($_POST["password"] != NULL)){
+<?php
+$user=0;
+$req=$bdd->query('SELECT * FROM users');
+while($donnees=$req->fetch()){
+	if (($_POST["login"]==$donnees["username"])&&($_POST["password"]==$donnees["password"])) {$user=1;}
+}
+if(($_POST["login"] != NULL) && ($_POST["password"] != NULL) && ($user==1)){
 $_SESSION["id"]= "5";
 $_SESSION["login"]=$_POST["login"] ;
 $_SESSION["password"]=$_POST["password"] ;

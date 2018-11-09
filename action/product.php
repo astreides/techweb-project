64905bcf65d2081376842f3341907e84889c2f31
Louis THIÉRY <louis.thiery@isen.yncrea.fr>
2018-11-09 17:09:13 +0100

    mis a jour cart

--- action/product.php
+++ action/product.php
@@ -1,11 +1,17 @@
 	<?php 
 	//action
 	$reponse=$bdd->query('SELECT * FROM products WHERE name=\''.$_POST["product"].'\'');
-
 	$donnees=$reponse->fetch();
+	$requete='SELECT * FROM order_products WHERE product_id=\''.$donnees["id"].'\'';
+	$cart=$bdd->query($requete);
+	$i =0;
 	if( isset($_POST["quantity"]) ){
-        $req = $bdd->query('INSERT INTO `order_products` (order_id, product_id, quantity, unit_price) VALUES (\''.$_POST["num_cart"].'\',\''.$donnees['id'].'\',\''.$_POST["quantity"].'\',\''.$donnees['unit_price'].'\')');
-
-		
-    }
+		while($panier =$cart->fetch()){
+			$req = $bdd->query('UPDATE `order_products` set `quantity`= `quantity`+\''.$_POST["quantity"].'\' WHERE product_id=\''.$donnees["id"].'\'AND order_id=4');
+			$i=1;
+		}
+		if($i==0){
+		$req = $bdd->query('INSERT INTO `order_products` (order_id, product_id, quantity, unit_price) VALUES (\''.$_POST["num_cart"].'\',\''.$donnees['id'].'\',\''.$_POST["quantity"].'\',\''.$donnees['unit_price'].'\')');
+		}   
+	}
 	?>

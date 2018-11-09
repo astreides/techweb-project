c9c3f2c825751c4d6d14598c08b552e0a545b0c6
JustinDuban <justin.duban@free.fr>
2018-11-09 22:24:56 +0100

    sessionid

--- action/co.php
+++ action/co.php
@@ -3,12 +3,11 @@
 $user=0;
 $req=$bdd->query('SELECT * FROM users');
 while($donnees=$req->fetch()){
-	if (($_POST["login"]==$donnees["username"])&&($_POST["password"]==$donnees["password"])) {$user=1;}
+	if (($_POST["login"]==$donnees["username"])&&($_POST["password"]==$donnees["password"])) {$user=$donnees["id"];}
 }
-if(($_POST["login"] != NULL) && ($_POST["password"] != NULL) && ($user==1)){
-$_SESSION["id"]= "5";
+if(($_POST["login"] != NULL) && ($_POST["password"] != NULL) && ($user!=0)){
+$_SESSION["id"]= $user;
 $_SESSION["login"]=$_POST["login"] ;
 $_SESSION["password"]=$_POST["password"] ;
 }
-	
 ?>
